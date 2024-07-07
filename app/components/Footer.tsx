3965f69d7a99c4c5248aa41f68292aa091a559a1
abrahamvg <abrageor06@gmail.com>
2024-07-07 17:31:46 +0530

    fixed an issue in footer affecting responsiveness

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -47,7 +47,7 @@ export default function Footer() {
     <section
       className="md:px-28 px-16 flex lg:flex-row flex-col lg:justify-between justify-center md:items-center items-start py-12 lg:gap-3 gap-5 bg-neutral-black text-white"
     >
-      <div className="space-y-10">
+      <div className="space-y-10 max-lg:w-full">
         <Link
           href="/"
           className="flex flex-row items-center gap-2"
@@ -67,10 +67,10 @@ export default function Footer() {
           ))}
         </div>
       </div>
-      <div className="flex lg:flex-nowrap flex-wrap justify-between gap-10 min-w-96">
-        <div className="flex sm:flex-row gap-8 flex-col">
+      <div className="flex lg:flex-nowrap flex-wrap justify-between gap-10 items-start max-lg:w-full">
+        <div className="flex sm:flex-row gap-8 flex-col max-lg:w-full">
           {footerLinks.map((items) => (
-            <div className="flex flex-col gap-3 min-w-40" key={items.title}>
+            <div className="flex flex-col gap-3 lg:min-w-40 w-full" key={items.title}>
               <h5 className="font-semibold">{items.title}</h5>
               <ul className="flex flex-col font-light">
                 <li className="flex flex-col gap-2" role="button">
@@ -84,17 +84,17 @@ export default function Footer() {
             </div>
           ))}
         </div>
-        <form className="flex flex-col gap-2" onSubmit={(e)=>{e.preventDefault()}}>
+        <form className="flex flex-col gap-2 w-full" onSubmit={(e)=>{e.preventDefault()}}>
           <h5 className="font-semibold">Stay up to date</h5>
-          <div className="bg-neutral-white/10 py-2 px-3 rounded-md flex flex-row items-center">
+          <div className="bg-neutral-white/10 py-2 px-3 rounded-md flex flex-row items-center justify-between ">
             <input
               type="email"
-              className="text-light shadow-none bg-transparent"
+              className="text-light shadow-none bg-transparent w-full"
               id="emailSubmitInput"
               placeholder="Your email address"
             />
             <button>
-              <Image src="/send.svg" height={20} width={20} alt="Submit Button" className="my-auto" />
+              <Image src="/send.svg" height={20} width={20} alt="Submit Button"/>
             </button>
           </div>
         </form>
